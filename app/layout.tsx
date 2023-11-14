e38ca93612220749a8fefa2fe98e418af00bf6ca
MohammadAqsha <143048573+MohammadAqsha@users.noreply.github.com>
2023-11-15 02:02:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
+// app/layout.tsx
+
+import { ReactNode } from 'react';
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
 
 export const metadata = {
   title: 'Mohammad Aqsha Arianto Latif',
   description: 'Made for PMW 2023',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
